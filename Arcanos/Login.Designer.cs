d07ccc1716596a3c02dbdfafc5947ae8d1d3d042
aline.dorini <alineconchetta@gmail.com>
2025-05-02 03:30:47 -0300

    Adicionar arquivos de projeto.

---
+++ Arcanos/Login.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Arcanos
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.labelSenha = new System.Windows.Forms.Label();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.textBoxSenha = new System.Windows.Forms.TextBox();
+            this.buttonEntrar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelCadastro = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelUsuario
+            // 
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.labelUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsuario.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelUsuario.Location = new System.Drawing.Point(32, 203);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(64, 20);
+            this.labelUsuario.TabIndex = 0;
+            this.labelUsuario.Text = "Usuário";
+            // 
+            // labelSenha
+            // 
+            this.labelSenha.AutoSize = true;
+            this.labelSenha.BackColor = System.Drawing.Color.Transparent;
+            this.labelSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSenha.ForeColor = System.Drawing.Color.White;
+            this.labelSenha.Location = new System.Drawing.Point(32, 277);
+            this.labelSenha.Name = "labelSenha";
+            this.labelSenha.Size = new System.Drawing.Size(56, 20);
+            this.labelSenha.TabIndex = 1;
+            this.labelSenha.Text = "Senha";
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Location = new System.Drawing.Point(36, 226);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(301, 20);
+            this.textBoxUsuario.TabIndex = 2;
+            // 
+            // textBoxSenha
+            // 
+            this.textBoxSenha.Location = new System.Drawing.Point(36, 300);
+            this.textBoxSenha.Name = "textBoxSenha";
+            this.textBoxSenha.Size = new System.Drawing.Size(301, 20);
+            this.textBoxSenha.TabIndex = 3;
+            this.textBoxSenha.UseSystemPasswordChar = true;
+            // 
+            // buttonEntrar
+            // 
+            this.buttonEntrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.buttonEntrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEntrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(9)))), ((int)(((byte)(56)))));
+            this.buttonEntrar.Location = new System.Drawing.Point(129, 381);
+            this.buttonEntrar.Name = "buttonEntrar";
+            this.buttonEntrar.Size = new System.Drawing.Size(115, 34);
+            this.buttonEntrar.TabIndex = 4;
+            this.buttonEntrar.Text = "Entrar";
+            this.buttonEntrar.UseVisualStyleBackColor = false;
+            this.buttonEntrar.Click += new System.EventHandler(this.buttonEntrar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Arcanos.Properties.Resources.arcanos_sem_fundo__login_;
+            this.pictureBox1.Location = new System.Drawing.Point(93, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 158);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelCadastro
+            // 
+            this.labelCadastro.AutoSize = true;
+            this.labelCadastro.BackColor = System.Drawing.Color.Transparent;
+            this.labelCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCadastro.ForeColor = System.Drawing.Color.White;
+            this.labelCadastro.Location = new System.Drawing.Point(140, 429);
+            this.labelCadastro.Name = "labelCadastro";
+            this.labelCadastro.Size = new System.Drawing.Size(89, 18);
+            this.labelCadastro.TabIndex = 6;
+            this.labelCadastro.Text = "Cadastre-se";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(9)))), ((int)(((byte)(56)))));
+            this.BackgroundImage = global::Arcanos.Properties.Resources.background1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(384, 561);
+            this.Controls.Add(this.labelCadastro);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonEntrar);
+            this.Controls.Add(this.textBoxSenha);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Controls.Add(this.labelSenha);
+            this.Controls.Add(this.labelUsuario);
+            this.DoubleBuffered = true;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.Label labelSenha;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.TextBox textBoxSenha;
+        private System.Windows.Forms.Button buttonEntrar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelCadastro;
+    }
+}
---
+++ Arcanos/Login.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arcanos
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEntrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBoxUsuario.Text.Equals("Aline") && textBoxSenha.Text.Equals("123"))
+                {
+                    // Seguir login para área restrita do app.
+                }
+                else
+                {
+                    MessageBox.Show("ERRO - 401",
+                        "Desculpe. Usuário ou senha incorretos",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Desculpe",
+                        ex.Message,
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+
+            }
+        }
+    }
+}
